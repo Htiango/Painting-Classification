34b876b90000706036f11c9328c40f714445514b
Htiango <hongty106@gmail.com>
2018-04-20 02:54:37 -0400

    add the image crawler

---
+++ crawler/config.py
@@ -0,0 +1,26 @@
+
+
+# information about wikiart
+BASE_URL= "https://www.wikiart.org/en/"
+STYLE_URL_PREFIX = "paintings-by-style/"
+PAGINATION_URL_PREFIX = "?json=2&page="
+
+
+# kind of styles we want
+STYLES = ["gongbi", "gothic"]
+
+# timeout for WikiArt
+# SEE https://github.com/lucasdavid/wikiart/blob/master/wikiart/settings.py
+METADATA_REQUEST_TIMEOUT = 2 * 60
+PAINTINGS_REQUEST_TIMEOUT = 5 * 60
+
+
+STYLE_JSON = "../data/json/styles.json"
+JSON_PATH = "../data/json"
+
+IMAGE_PATH = "../data/paintings"
+
+
+PAGE_LIMIT = 20
+
+SAVE_IMAGES_IN_FORMAT = '.jpg'
---
+++ crawler/crawler.py
@@ -0,0 +1,91 @@
+from config import *
+import requests
+import json
+import os
+import argparse
+import shutil
+
+def retrieve_style_json():
+    print("retrieving styles json ... ")
+    dic = {}
+    for style in STYLES:
+        for page in range(1, PAGE_LIMIT):
+            url = BASE_URL + STYLE_URL_PREFIX + style + PAGINATION_URL_PREFIX + str(page)
+            try:
+                response = requests.get(url, timeout=METADATA_REQUEST_TIMEOUT)
+                res_json = response.json()['Paintings']
+                if res_json is None:
+                    break
+                dic[style] = res_json
+            except Exception as e:
+                print(str(e))
+                continue
+
+    with open(STYLE_JSON, 'w') as f:
+        json.dump(dic, f)
+    print("Generate json file!")
+
+
+def retrieve_image():
+    with open(STYLE_JSON, 'r') as f:
+        paints_styles_info = json.load(f)
+
+    num = sum([len(paints_styles_info[style]) for style in paints_styles_info.keys()])
+    
+    print('Totally image number: ' + str(num))
+    print("downloading images ... ")
+
+    count = 0
+    success = 0
+
+    for style in paints_styles_info.keys():
+        paths_infos = paints_styles_info[style]
+
+        os.makedirs(os.path.join(IMAGE_PATH, style), exist_ok=True)
+
+        for paints_info in paths_infos:
+            count += 1
+            url = paints_info["image"]
+            title = paints_info["title"]
+
+            try: 
+                response = requests.get(url, timeout=PAINTINGS_REQUEST_TIMEOUT, stream=True)
+
+                filepath = os.path.join(IMAGE_PATH, style, title + SAVE_IMAGES_IN_FORMAT)
+
+                # print(filepath)
+
+                with open(filepath, 'wb') as f:
+                    response.raw.decode_content = True
+                    shutil.copyfileobj(response.raw, f)
+                    print("["+str(count)  + "] - Save file: " + title)
+                    success += 1
+            except Exception as e:
+                print("["+str(count)  + "] - Fail")
+                print(str(e))
+
+    print("Finish downloading!")
+
+    print('Totally image number: ' + str(num))
+
+    print('Success number: ' + str(success))
+
+
+
+def main():
+    # parser = argparse.ArgumentParser()
+ #    parser.add_argument('-name', '--name', type=str, 
+ #        required=True, help='the name of the style json file.')
+ #    parser.add_argument("-m", "--mode", help = "select mode by 'train' or test",
+ #        choices = ["train", "test"], default = "test")
+    
+ #    args = parser.parse_args()
+
+    os.makedirs(IMAGE_PATH, exist_ok=True)
+    os.makedirs(JSON_PATH, exist_ok=True)
+    retrieve_style_json()
+    retrieve_image()
+
+
+if __name__ == "__main__":
+    main()
